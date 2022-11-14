f80c2872d6b06002733838641e858eb01c44bf4d
Matt Miller <mattmillerf@pm.me>
2022-11-14 08:35:56 -0500

    update next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  experimental: {appDir: true}
+}
+
+module.exports = nextConfig
