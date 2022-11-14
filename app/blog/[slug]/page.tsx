953e4181a87aed41bfe8d4d819da75d5547cbf45
Matt Miller <mattmillerf@pm.me>
2022-11-14 13:28:58 -0500

    refactor

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,45 +1,12 @@
-import { micromark } from "micromark";
-import { gfm, gfmHtml } from "micromark-extension-gfm";
-import { cache } from "react";
+import { getPosts, getPost, getMarkdown } from "../inc/markdown_api";
 import "../../../styles/globals.css";
-
-const getPosts = cache(async () => {
-  const res = await fetch(process.env.GITHUB_ENDPOINT, {
-    headers: {
-      Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-    },
-  });
-  console.log(res.json);
-  return res.json();
-});
-
-async function getPost(slug) {
-  const posts = await getPosts();
-  const post = posts.find((post) => post.name === slug);
-  return post;
-}
-
-async function markdownToHtml(markdown: string) {
-  const result = micromark(markdown, {
-    extensions: [gfm()],
-    htmlExtensions: [gfmHtml()],
-  });
-  return result;
-}
-
-async function getMarkdown(slug) {
-  const post = await getPost(slug);
-  const res = await fetch(post.download_url);
-  const markdown = await markdownToHtml(await res.text());
-  return markdown;
-}
 
 export default async function Page({ params }) {
   const post = await getPost(params.slug);
   const content = await getMarkdown(params.slug);
   return (
     <>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <article dangerouslySetInnerHTML={{ __html: content }} />
     </>
   );
 }
---
+++ app/blog/inc/markdown_api.ts
@@ -0,0 +1,36 @@
+import { cache } from "react";
+import { micromark } from "micromark";
+import { gfm, gfmHtml } from "micromark-extension-gfm";
+
+const getPosts = cache(async () => {
+    const res = await fetch(process.env.GITHUB_ENDPOINT, {
+      headers: {
+        Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+      },
+    });
+    console.log(res.json);
+    return res.json();
+});
+
+const getPost = async (slug: any) => {
+    const posts = await getPosts();
+    const post = posts.find((post: any) => post.name === slug);
+    return post;
+}
+
+const markdownToHtml = async(markdown: string) => {
+  const result = micromark(markdown, {
+    extensions: [gfm()],
+    htmlExtensions: [gfmHtml()],
+  });
+  return result;
+}
+
+const getMarkdown = async (slug: any) => {
+  const post = await getPost(slug);
+  const res = await fetch(post.download_url);
+  const markdown = await markdownToHtml(await res.text());
+  return markdown;
+}
+
+export {getPosts, getPost, getMarkdown}
