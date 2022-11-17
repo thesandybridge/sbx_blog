d1b772e004a84d8a4f55eaf6e4585d49227c7cc3
Matt Miller <mattmillerf@pm.me>
2022-11-16 23:22:48 -0500

    adding loading component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,28 +1,36 @@
 import { getPosts } from "../../utils/markdown_api";
 import Link from "next/link";
 
-export default async function Page() {
+const Page = async () => {
   const posts = await getPosts();
 
   return (
     <>
+      <h1>Blog, Guides, Notes</h1>
+      <p>
+        This is collection of blog posts on whatever I feel like writing about,
+        as well as some guides I wrote. I also keep some notes here for personal
+        use that I have marked public.
+      </p>
       <p>
         The blog posts are saved in a github private repo and fetched using the
         github API. I then parse the markdown with micromark in an internal API
         I created. You can view the source code on my github.
       </p>
-      {posts.map((post: any) => {
-        let title = post.name.replace(/\.[^/.]+$/, "");
-        return (
-          <>
-            <div>
+      <article className="post-archive">
+        {posts.map((post: any) => {
+          let title = post.name.replace(/\.[^/.]+$/, "");
+          return (
+            <>
               <Link href={`/blog/${title}`} title={title}>
                 <h2 className="blog-headline">{title}</h2>
               </Link>
-            </div>
-          </>
-        );
-      })}
+            </>
+          );
+        })}
+      </article>
     </>
   );
-}
+};
+
+export default Page;
