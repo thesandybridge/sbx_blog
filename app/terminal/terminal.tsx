976fb523d0280b269fd17d4f10d22935afc5443b
Matt Miller <mattmillerf@pm.me>
2022-11-20 17:42:35 -0500

    terminal logic

--- app/terminal/terminal.tsx
+++ app/terminal/terminal.tsx
@@ -1,10 +1,34 @@
+"use client";
+import { useState, useRef } from "react";
 import styles from "./Terminal.module.css";
 
 export default function Terminal() {
+  const cliInterface = useRef(null);
+
+  const [cli, setCli] = useState("");
+
+  const handleInput = (event) => {
+    setCli(event.target.value);
+  };
+
+  const handleFocus = () => {
+    cliInterface.current.focus();
+  };
+
   return (
     <>
       <div className={styles.terminal}>
-        <input type="text" name="interface" className={styles.interface} />
+        <textarea
+          ref={cliInterface}
+          autoFocus
+          name="interface"
+          onChange={handleInput}
+          className={styles.interface}
+        />
+        <div className={styles.commandLine} onClick={handleFocus}>
+          <span className={styles.prompt}>{cli}</span>
+          <b className={styles.cursor}>█</b>
+        </div>
       </div>
     </>
   );
