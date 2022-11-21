6637579ce42bd1e89ea987a61b2bf92d5cee0aaa
Matt Miller <mattmillerf@pm.me>
2022-11-21 09:35:38 -0500

    add type for Posts

--- utils/markdown_api.ts
+++ utils/markdown_api.ts
@@ -3,6 +3,19 @@ import { micromark } from "micromark";
 import { gfm, gfmHtml } from "micromark-extension-gfm";
 import {frontmatter, frontmatterHtml} from 'micromark-extension-frontmatter'
 
+type Post = {
+  name: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string;
+  type: string;
+  _links: object;
+}
+
 const getPosts = async () => {
     const res = await fetch(process.env.GITHUB_ENDPOINT, {
       headers: {
@@ -12,9 +25,9 @@ const getPosts = async () => {
     return res.json();
 };
 
-const getPost = async (slug: any) => {
+const getPost = async (slug: string) => {
     const posts = await getPosts();
-    const post = posts.find((post: any) => post.name === slug);
+    const post: Post = posts.find((post: Post) => post.name === slug);
     return post;
 }
 
@@ -26,7 +39,7 @@ const markdownToHtml = async(markdown: string) => {
   return result;
 }
 
-const getMarkdownByPost = async (slug: any) => {
+const getMarkdownByPost = async (slug: string) => {
   const post = await getPost(`${slug}.md`);
   const res = await fetch(post.download_url);
   const markdown = await markdownToHtml(await res.text());
@@ -35,7 +48,7 @@ const getMarkdownByPost = async (slug: any) => {
 
 const getMarkdown = async () => {
   const posts = await getPosts();
-  const payload = await Promise.all(posts.map(async (post: any) => {
+  const payload = await Promise.all(posts.map(async (post: Post) => {
     const res = await fetch(post.download_url);
     const markdown = await res.text()
     return markdown;
