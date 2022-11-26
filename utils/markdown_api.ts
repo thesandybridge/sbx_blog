115b8e201d6d9d7d38e748febb495a036f5333b2
Matt Miller <mattmillerf@pm.me>
2022-11-25 22:32:23 -0500

    add some comments

--- utils/markdown_api.ts
+++ utils/markdown_api.ts
@@ -1,7 +1,5 @@
-import { cache } from "react";
 import { micromark } from "micromark";
 import { gfm, gfmHtml } from "micromark-extension-gfm";
-import {frontmatter, frontmatterHtml} from 'micromark-extension-frontmatter'
 
 type Post = {
   name: string;
@@ -16,6 +14,10 @@ type Post = {
   _links: object;
 }
 
+/**
+ * Fetch all posts from the Github API.
+ * @returns 
+ */
 const getPosts = async () => {
     const res = await fetch(process.env.GITHUB_ENDPOINT, {
       headers: {
@@ -25,6 +27,11 @@ const getPosts = async () => {
     return res.json();
 };
 
+/**
+ * Get a post by slug from the Github API.
+ * @param slug string containing the slug of the post.
+ * @returns 
+ */
 const getPost = async (slug: string) => {
   const res = await fetch(`${process.env.GITHUB_ENDPOINT}/${slug}.md`, {
     headers: {
@@ -34,6 +41,11 @@ const getPost = async (slug: string) => {
   return res.json();
 }
 
+/**
+ * Parse mardown to HTML and return the html.
+ * @param markdown 
+ * @returns 
+ */
 const markdownToHtml = async(markdown: string) => {
   const result = micromark(markdown, {
     extensions: [gfm()],
@@ -42,6 +54,11 @@ const markdownToHtml = async(markdown: string) => {
   return result;
 }
 
+/**
+ * Fetch post by slug from the Github API and then convert it to html. Returns the html payload.
+ * @param slug string containing the slug of the post.
+ * @returns 
+ */
 const getMarkdownByPost = async (slug: string) => {
   const payload = await getPost(slug)
   .then(async (post: Post) => {
@@ -52,6 +69,10 @@ const getMarkdownByPost = async (slug: string) => {
   return payload;
 }
 
+/**
+ * Fetch all posts from the Github API and then convert it to html. Returns the html payload for each post.
+ * @returns 
+ */
 const getMarkdown = async () => {
   const posts = await getPosts();
   const payload = await Promise.all(posts.map(async (post: Post) => {
